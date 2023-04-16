21fb29ed618a2ada264f40e529e8d794fe23ca4f
金眉卲 <120027289+Jinmeishao@users.noreply.github.com>
2023-04-16 19:00:55 +0900

    20230416 MISO : Initial Setting

---
+++ src/main/java/esa/study/esastudy/EsaStudyApplication.java
@@ -0,0 +1,13 @@
+package esa.study.esastudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsaStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EsaStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/esa/study/esastudy/EsaStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package esa.study.esastudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsaStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
